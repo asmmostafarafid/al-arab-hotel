a16ab0ad908ae323989d3078883879e27c9cfdb7
rafid365.info@gmail.com <rafid365.info@gmail.com>
2021-04-25 23:15:36 +0600

    Yay yay booking Room

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBt9bdPWr8T1MmyfxgCAD9caGPg9INN6rg",
+    authDomain: "burj-al-arab-a21c4.firebaseapp.com",
+    projectId: "burj-al-arab-a21c4",
+    storageBucket: "burj-al-arab-a21c4.appspot.com",
+    messagingSenderId: "31545210870",
+    appId: "1:31545210870:web:5229b7651311b8178f39d7"
+  };
+
+export default firebaseConfig;
